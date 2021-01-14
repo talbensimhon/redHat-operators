8af2f4a3d39eab99776b11e08be84a75d16af224
talbensimhon <talbensimhon71@gmail.com>
2021-01-14 19:35:47 +0200

    Created operator API for Wordpress

--- wordpress-operator/main.go
+++ wordpress-operator/main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	wordpressprojectv1alpha1 "github.com/example-inc/wordpress-operator/apis/wordpress-project/v1alpha1"
+	wordpressprojectcontrollers "github.com/example-inc/wordpress-operator/controllers/wordpress-project"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(wordpressprojectv1alpha1.AddToScheme(scheme))
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&wordpressprojectcontrollers.WordpressReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("wordpress-project").WithName("Wordpress"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Wordpress")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
